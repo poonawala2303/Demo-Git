5cc1a0f1af63a7067eddadc9fde4a608d25836e0
poonawala2303 <mustafa.poonawala82@gmail.com>
2024-08-13 12:17:00 +0530

    Files Updated

---
+++ MaxWealth.java
@@ -0,0 +1,24 @@
+public class MaxWealth {
+    public static void main(String[] args) {
+
+    }
+
+    static int maximumWealth(int[][] accounts) {
+        // person = row
+        // account = col
+        int max = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length ; person++) {
+            // when you start a new column , take  new sum for that row
+            int rowkasum = 0;
+            for (int bankAccount = 0; bankAccount< accounts[person].length; bankAccount++){
+                rowkasum += accounts[person][bankAccount];
+            }
+            // here , we have answer of sum of accounts of person
+            // check with overall answer
+            if(rowkasum>max){
+                max = rowkasum;
+            }
+        }
+        return max;
+    }
+}
