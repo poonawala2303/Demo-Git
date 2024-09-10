cd4b4060e680796378fdc276e76ff5777f4d9b11
poonawala2303 <mustafa.poonawala82@gmail.com>
2024-09-10 12:59:38 +0530

    Test commit

--- MaxWealth.java
+++ MaxWealth.java
@@ -1,6 +1,6 @@
 public class MaxWealth {
     public static void main(String[] args) {
-        System.out.println("Hello from Github");
+        System.out.println("Hello!! Expt 4 ");
     }
 
     static int maximumWealth(int[][] accounts) {
