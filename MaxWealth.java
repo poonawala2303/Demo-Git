59ac434116652ec88175dc0804f2bfe42ed0b589
Mustafa Poonawala <77799091+poonawala2303@users.noreply.github.com>
2024-09-10 12:53:14 +0530

    Update MaxWealth.java

--- MaxWealth.java
+++ MaxWealth.java
@@ -1,6 +1,6 @@
 public class MaxWealth {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 1234");
     }
 
     static int maximumWealth(int[][] accounts) {
